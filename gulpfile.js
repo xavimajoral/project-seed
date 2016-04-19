792b31fb651586b4b64b50fb5e88d50333f23972
xaviMajoral <xavi.majoral@hellopay.com>
2016-04-19 15:56:13 +0700

    Concatenated JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 /*
 gulp.task('log', function() {
@@ -9,10 +10,22 @@ gulp.task('log', function() {
 */
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = [
+	'components/scripts/pixgrid.js',
+	'components/scripts/rclick.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true })
 			.on('error', gutil.log))
 		.pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('scripts.js'))
+		.pipe(gulp.dest('builds/development/js'))
 });
